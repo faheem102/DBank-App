4a17526dae9d535e92dee33a2ebc227a02cba4f3
Faheem Hassan <faheemhassan1985@gmail.com>
2023-08-24 01:04:46 +0200

    added javascript with topup function

---
+++ src/dbank_assets/src/index.js
@@ -0,0 +1,28 @@
+import { dbank } from "../../declarations/dbank";
+
+window.addEventListener("load", async function () {
+  const currentAmount = await dbank.checkBalance();
+  document.getElementById("value").innerText =
+    Math.round(currentAmount * 100) / 100;
+});
+document
+  .querySelector("form")
+  .addEventListener("submit", async function (event) {
+    event.preventDefault();
+    const button = event.target.querySelector("#submit-btn");
+    const inputAmount = parseFloat(
+      document.getElementById("input-amount").value
+    );
+    const outputAmout = parseFloat(
+      document.getElementById("withdrawal-amount").value
+    );
+    button.setAttribute("disabled", true);
+    if (document.getElementById("input-amount").value.length != 0) {
+      await dbank.topUp(inputAmount);
+    }
+    const currentAmount = await dbank.checkBalance();
+    document.getElementById("value").innerText =
+      Math.round(currentAmount * 100) / 100;
+    document.getElementById("input-amount").value = "";
+    button.removeAttribute("disabled");
+  });
