7a60555f11fd58bc308c1caa9d47146470368a21
Faheem Hassan <faheemhassan1985@gmail.com>
2023-08-24 01:15:05 +0200

    withdraw function

--- src/dbank_assets/src/index.js
+++ src/dbank_assets/src/index.js
@@ -13,16 +13,21 @@ document
     const inputAmount = parseFloat(
       document.getElementById("input-amount").value
     );
-    const outputAmout = parseFloat(
+    const outputAmount = parseFloat(
       document.getElementById("withdrawal-amount").value
     );
     button.setAttribute("disabled", true);
     if (document.getElementById("input-amount").value.length != 0) {
       await dbank.topUp(inputAmount);
     }
+    if (document.getElementById("withdrawal-amount").value.length != 0) {
+      await dbank.withdrawl(outputAmount);
+    }
+    await dbank.compound();
     const currentAmount = await dbank.checkBalance();
     document.getElementById("value").innerText =
       Math.round(currentAmount * 100) / 100;
     document.getElementById("input-amount").value = "";
+    document.getElementById("withdrawal-amount").value = "";
     button.removeAttribute("disabled");
   });
